c0dea70ccfb6f47d7b6e03d2c2e31b64be2eb0b9
AlAminAshik <106368872+AlAminAshik@users.noreply.github.com>
2025-03-28 16:25:39 +0600

    configured platformini for blackpill and digital/serial read code added

--- src/Blink.cpp
+++ src/Blink.cpp
@@ -1,14 +1,14 @@
-#include <Arduino.h>
+// #include <Arduino.h>
 
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(PC13, OUTPUT);
-}
+// void setup() {
+//   // initialize digital pin LED_BUILTIN as an output.
+//   pinMode(PC13, OUTPUT);    //blue led connected to PC13
+// }
 
-// the loop function runs over and over again forever
-void loop() {
-  digitalWrite(PC13, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(500);                      // wait for a second
-  digitalWrite(PC13, LOW);   // turn the LED off by making the voltage LOW
-  delay(500);                      // wait for a second
-}
+// // the loop function runs over and over again forever
+// void loop() {
+//   digitalWrite(PC13, HIGH);  // turn the LED on (HIGH is the voltage level)
+//   delay(1000);                      // wait for a second
+//   digitalWrite(PC13, LOW);   // turn the LED off by making the voltage LOW
+//   delay(1000);                      // wait for a second
+// }
---
+++ src/Serial_digitalRead.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+
+void setup() {
+    //all pin are 5V tolerant on black pill except PA0 and PB5
+    Serial.begin(9600);
+  pinMode(PB1, INPUT_PULLDOWN);   //button connected to PB1 blackpill
+  pinMode(PB2, INPUT_PULLDOWN);   //button connected to PB2 blackpill
+  pinMode(PC13, OUTPUT);    //blue led connected to PC13    blackpill
+}
+
+void loop(){
+    int buttonState1 = digitalRead(PB1);
+    int buttonState2 = digitalRead(PB2);
+    if (buttonState1 == HIGH) {
+        digitalWrite(PC13, HIGH);
+    }
+    if (buttonState2 == HIGH) {
+        digitalWrite(PC13, LOW);
+    }
+    delay(10);
+    Serial.print("Button PB1: ");
+    Serial.print(buttonState1);
+    Serial.print(" Button PB2: ");
+    Serial.println(buttonState2);
+    Serial.println("LED state: " + String(digitalRead(PC13)));
+}
