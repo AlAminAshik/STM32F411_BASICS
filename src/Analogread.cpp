cf08e8e5ece6ba807ef51cfa593a6bb0fcbf3b01
AlAminAshik <106368872+AlAminAshik@users.noreply.github.com>
2025-03-28 17:37:46 +0600

    external interrup works

--- src/Analogread.cpp
+++ src/Analogread.cpp
@@ -1,15 +1,16 @@
-//STM32F411 supports 12 bit ADC resolution
-#include <Arduino.h>
+// //3.3V-GND works better than 5V for analogRead
+// //STM32F411 supports 12 bit ADC resolution
+// #include <Arduino.h>
 
-void setup() {
-    Serial.begin(115200);
-    analogReadResolution(12); // Set ADC resolution to 12 bits
-    pinMode(PB1, INPUT_ANALOG); //PA1 is an analog pin on the blackpill
-}
+// void setup() {
+//     Serial.begin(115200);
+//     analogReadResolution(12); // Set ADC resolution to 12 bits
+//     pinMode(PB1, INPUT_ANALOG); //PA1 is an analog pin on the blackpill
+// }
 
-void loop() {
-    int sensorValue = analogRead(PB1);
-    Serial.println(sensorValue);
-    delay(100);
-}
+// void loop() {
+//     int sensorValue = analogRead(PB1);
+//     Serial.println(sensorValue);
+//     delay(100);
+// }
 
