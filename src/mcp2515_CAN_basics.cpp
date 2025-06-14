55e4e70604423f43c3ef43ddd225ab467fc6d5e5
AlAminAshik <alaminashik890.bd@gmail.com>
2025-06-14 12:37:32 +0600

    .

--- src/mcp2515_CAN_basics.cpp
+++ src/mcp2515_CAN_basics.cpp
@@ -1,27 +1,60 @@
-#include <Arduino.h>
-#include <SPI.h>
-#include <mcp_can.h>
+//for using stm32 board change the pinout to PA4 and PA3 under MCP2515.h
+// #define MCP2515_DEFAULT_CS_PIN          PA4 //10
+// #define MCP2515_DEFAULT_INT_PIN         PA3 //2
 
-#define SPI_CS_PIN PA4
+#include <CAN.h>
 
-MCP_CAN CAN(SPI_CS_PIN);
+void onReceive(int packetSize) {
+  // received a packet
+  Serial.print("Received ");
+
+  if (CAN.packetExtended()) {
+    Serial.print("extended ");
+  }
+
+  if (CAN.packetRtr()) {
+    // Remote transmission request, packet contains no data
+    Serial.print("RTR ");
+  }
+
+  Serial.print("packet with id 0x");
+  Serial.print(CAN.packetId(), HEX);
+
+  if (CAN.packetRtr()) {
+    Serial.print(" and requested length ");
+    Serial.println(CAN.packetDlc());
+  } else {
+    Serial.print(" and length ");
+    Serial.println(packetSize);
+
+    // only print packet data for non-RTR packets
+    while (CAN.available()) {
+      Serial.print((char)CAN.read());
+    }
+    Serial.println();
+  }
+
+  Serial.println();
+}
+
 
 void setup() {
-  Serial.begin(115200);
-  while (CAN_OK != CAN.begin(MCP_STDEXT, CAN_500KBPS, MCP_8MHZ)) {
-    Serial.println("CAN init failed, retrying...");
-    delay(100);
+  Serial.begin(115200); // initialize serial communication at 115200 baud
+  while (!Serial);
+
+  Serial.println("CAN Receiver Callback");
+
+  // start the CAN bus at 500 kbps
+  if (!CAN.begin(500E3)) {
+    Serial.println("Starting CAN failed!");
+    while (1);
   }
-  Serial.println("CAN init successful!");
+
+  // register the receive callback
+  CAN.onReceive(onReceive);
+  Serial.println("CAN Receiver Callback registered");
 }
 
 void loop() {
-  const char msg[] = "Hello";
-  byte sndStat = CAN.sendMsgBuf(0x100, 0, sizeof(msg), (byte *)msg);
-  if (sndStat == CAN_OK) {
-    Serial.println("Message Sent Successfully!");
-  } else {
-    Serial.println("Error Sending Message...");
-  }
-  delay(1000);
+  // do nothing
 }
